5e2a4924d5e7d90762cd65df4f3b947bd2f59b7b
Daniel Soney <daniel.m.soney@student.uts.edu.au>
2026-06-30 16:11:02 +1000

    Inital Spring Boot project setup

---
+++ src/main/java/com/jysydney/gamesnight/GamesnightApplication.java
@@ -0,0 +1,13 @@
+package com.jysydney.gamesnight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesnightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesnightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jysydney/gamesnight/GamesnightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jysydney.gamesnight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesnightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
